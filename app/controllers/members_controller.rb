57983a54f5a8e493d699ec97ccbfaa4506cc6208
imubarak234 <imubarak2424@gmail.com>
2022-07-11 09:27:01 +0100

    progress

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,4 +3,27 @@ class MembersController < ApplicationController
   def index
     @member = Member.where(user: current_user)
   end
+
+  def new
+    @member_new = current_user.members.new
+    respond_to do |format|
+      format.html { render :new, locals: { members: @member_new } }
+    end
+  end
+
+  def create
+    member_create = Member.new(params.require(:member).permit(:full_name, :member_role, :email, :phone_number, :active_region))
+    member_create.update(user_id: current_user.id)
+    
+
+    respond_to do |format|
+      if member_create.save
+        flash[:notice] = 'Member Created'
+        redirect_to members_path
+      else
+        flash[:notice] = 'Error: Member not created'
+        render :new, locals: { members: @member_new }
+      end
+    end
+  end
 end
