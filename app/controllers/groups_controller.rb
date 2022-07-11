5de4ace9ffc94142bceb2db0bee777e5567ca88d
imubarak234 <imubarak2424@gmail.com>
2022-07-11 01:59:21 +0100

    controllers

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,10 @@
+class GroupsController < ApplicationController
+  # before_action :authenticate_user!
+  def index
+    # @group = Group.where(user: current_user)
+    # @user = current_user
+    # puts current_user
+  end
+
+  def show; end
+end
