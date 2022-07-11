4c8ed92a237c2e9d10f21e471ae2a6cb37a16a1c
imubarak234 <imubarak2424@gmail.com>
2022-07-11 01:54:04 +0100

    models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :members
+  has_one :groups
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :email, presence: true
 end
