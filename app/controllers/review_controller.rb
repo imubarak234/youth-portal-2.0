f735e9cbb9229b39dacd51483110635bd2d3591e
imubarak234 <imubarak2424@gmail.com>
2022-07-16 17:13:00 +0100

    is verfied

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -4,15 +4,28 @@ class ReviewController < ApplicationController
     @member = Member.where(user: current_user)
   end
 
-  def verified
+  def is_verified
     var = Member.where(user: current_user)
-    count = []
-    count1 = []
-    count2 = []
-    count3 = []
+    count_charmain = []
+    count_vice = []
+    count_secretary = []
+    count_legal = []
 
     var.each do |n|
+      if n.member_role == "Chairman"
+        count_charmain.push(1)
+      elsif n.member_role == "Vice Chairman"
+        count_vice.push(1)
+      elsif n.member_role == "General Secretary"
+        count_secretary.push(1)
+      elsif n.member_role == "Legal Advisor"
+        count_legal.push(1)
+      end
 
+      count_charmain.length > 0 && count_vice.length > 0 && count_secretary.length > 0 && count_legal.length > 0
     end
+  end
+
+  def verified
   end
 end
