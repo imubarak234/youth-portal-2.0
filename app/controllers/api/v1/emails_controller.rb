25e388fb7343a570928ea5f633b815c4fa215e38
imubarak234 <imubarak2424@gmail.com>
2022-09-28 18:23:26 +0100

    mailing

--- app/controllers/api/v1/emails_controller.rb
+++ app/controllers/api/v1/emails_controller.rb
@@ -1,8 +1,26 @@
 module Api 
   module V1
     class EmailsController < ApplicationController
+
+      require 'sendgrid-ruby'
+      include SendGrid
+
       def create
-        render json: { success: "It's working" }
+
+        from = Email.new(email: 'imubarak2424@gmail.com')
+        to = Email.new(email: 'shithappens242@gmail.com')
+        subject = 'Testing the api'
+        content = Content.new(type: 'text/plain', value: 'Testing the api if it works')
+        mail = Mail.new(from, subject, to, content)
+
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+        response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+
+        render json: response.status_code
+        render json: { keys: "#{ENV['SENDGRID_API_KEY']}",
+        
+       }
       end
     end
   end
